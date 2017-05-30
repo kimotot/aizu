02448deade9aa9738acc7e20bdb2fd9d471ed705
kimotot <god4bid@hear.to>
2017-05-30 14:06:51 +0900

    alds1_4_d　○ バイナリサーチによりｐを探索

--- alds/alds1_4_d.py
+++ alds/alds1_4_d.py
@@ -1,6 +1,42 @@
-[n, k] = [int(x) for x in input().split(" ")]
-ps = []
-for _ in range(n):
-    ps.append(int(input()))
+def decode():
+    [n, k] = [int(x) for x in input().split(" ")]
+    ws = []
+    for _ in range(n):
+        ws.append(int(input()))
 
-count = 0
+    return n, k, ws
+
+
+def check(k, ws, p):
+    sum_w = ws[0]
+    tr = 1
+
+    for w in ws[1:]:
+        if sum_w + w <= p:
+            sum_w += w
+        else:
+            sum_w = w
+            tr += 1
+
+            if tr > k:
+                return False
+
+    return True
+
+
+if __name__ == '__main__':
+    n, k, ws = decode()
+
+    lo = max(ws)
+    hi = sum(ws)
+
+    while lo <= hi:
+        p = (lo + hi) // 2
+
+        if check(k, ws, p):
+            hi = p - 1
+            ans = p
+        else:
+            lo = p + 1
+
+    print(ans)
