37b0b0f9e964f4549b5e2b2b658ef92068ea2917
kimotot <god4bid@hear.to>
2017-06-30 19:31:04 +0900

    alds1_13_a

---
+++ alds/alds1_12_c.py
@@ -0,0 +1,67 @@
+class Node:
+
+    def __init__(self, idx):
+        self.idx = idx
+        self.selected = False
+        self.cost = 1000000
+        self.prev = None
+        self.next = []
+
+    def set_next(self, args):
+        for i in range(args[0]):
+            self.next.append((args[i*2+1], args[i*2+2]))
+
+    def debug_disp(self):
+        print(self.idx)
+        print(self.selected)
+        print(self.cost)
+        print(self.next)
+        print("")
+
+    def disp(self):
+        print("{0} {1}".format(self.idx, self.cost))
+
+class Dijkstra:
+
+    def __init__(self, n):
+        self.n = n
+        self.nodes = [Node(i) for i in range(n)]
+
+    def initialize(self, s):
+        self.nodes[s].selected = False
+        self.nodes[s].cost = 0
+
+    def update(self):
+        ns = list(filter(lambda n: not n.selected, self.nodes))
+
+        while ns:
+            unode = min(ns, key=lambda n: n.cost)
+            unode.selected = True
+            for (v, d) in unode.next:
+                vnode = self.nodes[v]
+                if vnode.cost > unode.cost + d:
+                    vnode.cost = unode.cost + d
+                    vnode.prev = unode.idx
+            ns = list(filter(lambda n: not n.selected, self.nodes))
+
+
+def decode():
+    n = int(input())
+    tree = Dijkstra(n)
+
+    for _ in range(n):
+        al = [int(x) for x in input().split()]
+        tree.nodes[al[0]].set_next(al[1:])
+
+    return n, tree
+
+
+if __name__ == '__main__':
+
+    n, tree = decode()
+
+    tree.initialize(0)
+    tree.update()
+
+    for node in tree.nodes:
+        node.disp()
---
+++ alds/alds1_13_a.py
@@ -0,0 +1,42 @@
+QUEEN = 1
+EMPTY = 0
+directions = {(r, c) for r in range(-1, 2) for c in range(-1, 2) if not(r == 0 and c == 0)}
+
+def decode():
+    board = [[EMPTY] * 8 for _ in range(8)]
+
+    n = int(input())
+    for _ in range(n):
+        [r, c] = [int(x) for x in input().split()]
+        board[r][c] = QUEEN
+
+    return board
+
+
+def disp_board(board):
+    for r in board:
+        str = ""
+        for c in r:
+            if c == QUEEN:
+                str += "Q"
+            else:
+                str += "."
+        print(str)
+
+
+def isput(board, r, c):
+    for (rd, cd) in directions:
+        rn = r + rd
+        cn = c + cd
+        while 0 <= rn < 8 and 0 <= cn < 8:
+            if board[rn][cn] == QUEEN:
+                return False
+            rn += rd
+            cn += cd
+
+    return True
+
+if __name__ == '__main__':
+
+    board = decode()
+    disp_board(board)
